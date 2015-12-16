1f49d7615cfde635bc08ed3ff2a52011d048b4b8
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-16 13:00:34 +0530

    added temperature program

---
+++ JavaPrograms/src/Naushad/temperature.java
@@ -0,0 +1,13 @@
+package Naushad;
+
+public class temperature {
+
+	public static void main(String[] args) {
+		
+		double temp = 32.5;
+
+		System.out.print("The Temperature today is ");
+		System.out.print(temp);
+		System.out.println("° Celsius");
+	}
+}
