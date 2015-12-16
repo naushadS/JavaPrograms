1a3c00a6db17417d90ae0192211d56450939fab5
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-16 13:26:47 +0530

    now it takes input from user!

--- JavaPrograms/src/Naushad/temperature.java
+++ JavaPrograms/src/Naushad/temperature.java
@@ -1,10 +1,16 @@
 package Naushad;
 
+import java.util.Scanner;
+
 public class temperature {
 
 	public static void main(String[] args) {
+		double temp;
 		
-		double temp = 32.5;
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter todays temperature nigga");
+		temp=input.nextDouble();
+		
 
 		System.out.print("The Temperature today is ");
 		System.out.print(temp);
