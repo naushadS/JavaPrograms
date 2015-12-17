1d60c7a7379ca7cdf3730b49e250135efa0fc58c
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-17 23:14:11 +0530

    implemented while loop

--- JavaPrograms/src/Naushad/basiccalculator.java
+++ JavaPrograms/src/Naushad/basiccalculator.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class basiccalculator {
 
 	public static void main(String[] args) {
-		int num1, num2, result;
+		int num1, num2, result,counter=0;
 
 		Scanner input=new Scanner(System.in);
 
@@ -17,6 +17,13 @@ public class basiccalculator {
 		num2 -= 1; //testing out decrement operator
 		result=num1+num2;
 		System.out.println("The Result is "+result);
+		System.out.println("counting zero to result");
+		
+		while(counter<=result){
+			System.out.println(counter);
+			counter++;
+			
+		}
 	}
 
 }
