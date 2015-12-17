727fb142bc604f5c2260950a68b998826e8d868b
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-17 12:00:28 +0530

    testing increment,decrement operator

--- JavaPrograms/src/Naushad/basiccalculator.java
+++ JavaPrograms/src/Naushad/basiccalculator.java
@@ -11,8 +11,10 @@ public class basiccalculator {
 
 		System.out.println("Enter first number");
 		num1=input.nextInt();
+		num1 += 1; //testing out increment operator
 		System.out.println("Enter second number");
 		num2=input.nextInt();
+		num2 -= 1; //testing out decrement operator
 		result=num1+num2;
 		System.out.println("The Result is "+result);
 	}
