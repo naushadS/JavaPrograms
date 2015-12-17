0408809362f8e1b994738e49014b7a115304898a
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-17 15:06:18 +0530

    fixed indentation

--- JavaPrograms/src/Naushad/payday.java
+++ JavaPrograms/src/Naushad/payday.java
@@ -6,27 +6,28 @@ public class payday {
 
 	public static void main(String[] args) {
 		int week;
-		
+
 		Scanner input=new Scanner(System.in);
-		
+
 		System.out.println("Enter Week Number(of current month)");
 		week=input.nextInt();
-		
+
 		switch(week){
-			case 1:
-				System.out.println("your are 3 weeks away from getting salary");
-                break;		
-			case 2:
-				System.out.println("your are 2 weeks away from getting salary");
-                break;	
-			case 3:
-				System.out.println("your are 1 week away from getting salary");
-                break;	
-			case 4:
-				System.out.println("you will be getting salary by the end of this week");
-                break;	
-           default:
-        	   System.out.println("wrong week number entered.Try again");
+		case 1:
+			System.out.println("your are 3 weeks away from getting salary");
+			break;		
+		case 2:
+			System.out.println("your are 2 weeks away from getting salary");
+			break;	
+		case 3:
+			System.out.println("your are 1 week away from getting salary");
+			break;	
+		case 4:
+			System.out.println("you will be getting salary by the end of this week");
+			break;	
+		default:
+			System.out.println("wrong week number entered.Try again");
+			break;
 		}
 
 	}
