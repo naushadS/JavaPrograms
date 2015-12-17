f12eb61081f5d5fef4818678516d58bc77ee4421
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-17 23:37:36 +0530

    methods that takes in argument

--- JavaPrograms/src/Naushad/company.java
+++ JavaPrograms/src/Naushad/company.java
@@ -1,13 +1,23 @@
 package Naushad;
 
+import java.util.Scanner;
+
 public class company {
 
 	public static void main(String[] args) {
+		int yearfounded;
+		
+		Scanner input=new Scanner(System.in);
+		
 		System.out.println("*****************");
 		System.out.println("ABC Company Private Ltd:");
 
 		companydetails cd=new companydetails();
-		cd.yearfounded();
+		
+		System.out.println("what year was this company founded?");
+		yearfounded=input.nextInt();
+		
+		cd.yearfounded(yearfounded);
 		cd.founder();
 		System.out.println("*****************");
 	}
--- JavaPrograms/src/Naushad/companydetails.java
+++ JavaPrograms/src/Naushad/companydetails.java
@@ -1,8 +1,8 @@
 package Naushad;
 
 public class companydetails {
-	public void yearfounded(){
-		System.out.println("Founded:1995");
+	public void yearfounded(int yearfounded){
+		System.out.println("Founded:"+yearfounded);
 	}
 	public void founder(){
 		System.out.println("Founder:Naushad Shukoor");
