77cb72d8c9080815e4160723c87c7b87483b9328
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-17 14:20:56 +0530

    tested logical operator

--- JavaPrograms/src/Naushad/temperature.java
+++ JavaPrograms/src/Naushad/temperature.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class temperature {
 
 	public static void main(String[] args) {
-		double temp;
+		double temp,bodytemp=37;
 		
 		Scanner input=new Scanner(System.in);
 		System.out.println("Enter todays temperature nigga");
@@ -16,10 +16,12 @@ public class temperature {
 		System.out.print(temp);
 		System.out.println("° Celsius");
 		
-		if(temp<50){
-			System.out.println("you can survive in this weather");
+		if(temp<0 || temp>50){
+			System.out.println("you may die due to extreme weather conditions");
 		}else{
-			System.out.println("you may die due to heat");
+			System.out.println("you can survive in this weather");
 		}
+		
+		
 	}
 }
