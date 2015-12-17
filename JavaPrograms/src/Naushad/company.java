3de40bfe0ca6da1316a2a2ba0bc6282d9a17b3d7
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-17 23:22:46 +0530

    implemented multiple classes

---
+++ JavaPrograms/src/Naushad/company.java
@@ -0,0 +1,15 @@
+package Naushad;
+
+public class company {
+
+	public static void main(String[] args) {
+		System.out.println("*****************");
+		System.out.println("ABC Company Private Ltd:");
+
+		companydetails cd=new companydetails();
+		cd.yearfounded();
+		cd.founder();
+		System.out.println("*****************");
+	}
+
+}
---
+++ JavaPrograms/src/Naushad/companydetails.java
@@ -0,0 +1,11 @@
+package Naushad;
+
+public class companydetails {
+	public void yearfounded(){
+		System.out.println("Founded:1995");
+	}
+	public void founder(){
+		System.out.println("Founder:Naushad Shukoor");
+	}
+
+}
