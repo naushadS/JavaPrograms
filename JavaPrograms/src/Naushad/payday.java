95c6abbe6dfdb0ae913b595a84dfe6f5c40cdc0b
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-17 15:03:00 +0530

    implemented switch condition statement

---
+++ JavaPrograms/src/Naushad/payday.java
@@ -0,0 +1,33 @@
+package Naushad;
+
+import java.util.Scanner;
+
+public class payday {
+
+	public static void main(String[] args) {
+		int week;
+		
+		Scanner input=new Scanner(System.in);
+		
+		System.out.println("Enter Week Number(of current month)");
+		week=input.nextInt();
+		
+		switch(week){
+			case 1:
+				System.out.println("your are 3 weeks away from getting salary");
+                break;		
+			case 2:
+				System.out.println("your are 2 weeks away from getting salary");
+                break;	
+			case 3:
+				System.out.println("your are 1 week away from getting salary");
+                break;	
+			case 4:
+				System.out.println("you will be getting salary by the end of this week");
+                break;	
+           
+		}
+
+	}
+
+}
