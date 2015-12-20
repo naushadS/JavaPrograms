d35b4f9bcf8a10c3f6c14693ef13705596056866
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-20 18:07:34 +0530

    implemented math class methods

---
+++ JavaPrograms/src/Naushad/mathClassMethods.java
@@ -0,0 +1,17 @@
+package Naushad;
+
+public class mathClassMethods {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.abs(-126.5));
+		System.out.println(Math.abs(126.5));
+		System.out.println(Math.floor(7.5));
+		System.out.println(Math.ceil(7.5));
+		System.out.println(Math.max(7.8, 8.7));
+		System.out.println(Math.min(7.8, 8.7));
+		System.out.println(Math.pow(8, 2));
+		System.out.println(Math.sqrt(9));
+	}
+
+}
