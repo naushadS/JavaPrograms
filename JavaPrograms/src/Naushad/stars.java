3d63970672f6af7b4b0329f9dfb8d152659a8aeb
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-20 10:53:33 +0530

    star pattern program

--- JavaPrograms/src/Naushad/stars.java
+++ JavaPrograms/src/Naushad/stars.java
@@ -4,7 +4,10 @@ public class stars {
 
 	public static void main(String[] args) {
 		for(int i=0;i<5;i++){
-			System.out.println("*");
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 }
