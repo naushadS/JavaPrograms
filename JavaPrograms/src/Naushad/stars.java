01e8bceec3324ebe125361257bd6278225b79a4b
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-20 12:15:44 +0530

    added more star patterns

--- JavaPrograms/src/Naushad/stars.java
+++ JavaPrograms/src/Naushad/stars.java
@@ -3,9 +3,97 @@ package Naushad;
 public class stars {
 
 	public static void main(String[] args) {
-		for(int i=0;i<5;i++){
-			for(int j=0;j<=i;j++){
+		
+		 /* 
+		 Output:
+		 
+		 *
+		 **
+		 ***
+		 ****
+		 *****
+		 
+		 */
+		 
+		for(int rows=1;rows<=5;rows++){
+			for(int numberOfStars=1;numberOfStars<=rows;numberOfStars++){
 				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		/* 
+		 Output:
+		 
+		     *
+		    **
+		   ***
+		  ****
+		 *****
+		 
+		 */
+		 
+		for(int rows=1;rows<=5;rows++){
+			for(int spaces=1;spaces<=(5-rows);spaces++){
+				System.out.print(" ");
+			}
+			for(int numberOfStars=1;numberOfStars<=rows;numberOfStars++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		/* 
+		 Output:
+		 
+              * 
+             * * 
+            * * * 
+           * * * * 
+          * * * * * 
+		 
+		 */
+		
+		for(int rows=1;rows<=5;rows++){
+			for(int spaces=1;spaces<=(5-rows);spaces++){
+				System.out.print(" ");
+			}
+			for(int numberOfStars=1;numberOfStars<=rows;numberOfStars++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		/* 
+		 Output:
+		 
+             * 
+            * * 
+           * * * 
+          * * * * 
+         * * * * * 
+          * * * * 
+           * * * 
+            * * 
+             * 
+		 
+		 */
+		
+		for(int rows=1;rows<=5;rows++){
+			for(int spaces=1;spaces<=(5-rows);spaces++){
+				System.out.print(" ");
+			}
+			for(int numberOfStars=1;numberOfStars<=rows;numberOfStars++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for(int rows=4;rows>=1;rows--){
+			for(int spaces=1;spaces<=(5-rows);spaces++){
+				System.out.print(" ");
+			}
+			for(int numberOfStars=1;numberOfStars<=rows;numberOfStars++){
+				System.out.print("* ");
 			}
 			System.out.println();
 		}
