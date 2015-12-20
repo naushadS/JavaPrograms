c3685ad174f92318201af8917684b9d1d4e49312
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-20 18:17:49 +0530

    tested in-built random class

--- JavaPrograms/src/Naushad/helloworld.java
+++ JavaPrograms/src/Naushad/helloworld.java
@@ -1,8 +1,17 @@
 package Naushad;
 
+import java.util.Random;
+
 public class helloworld {
 
 	public static void main(String[] args) {
+		int luckyNumber;
+		
 		System.out.println("Hello World!"); //prints message on screen
+		
+		Random rand=new Random();
+		luckyNumber=1+rand.nextInt(99);
+		System.out.println("Your lucky number for today:"+luckyNumber); //prints out users lucky number
+		
 	}
 }
