6463342a9cb332e5592ff42ebcf0c96fca2ae38c
Naushad Shukoor <naushadshukoor@gmail.com>
2015-12-30 00:09:49 +0530

    enabled user interaction initially

--- JavaPrograms/src/Naushad/freqOfDice.java
+++ JavaPrograms/src/Naushad/freqOfDice.java
@@ -1,16 +1,20 @@
 package Naushad;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class freqOfDice {
 
 	public static void main(String[] args) {
 		int sum=0;
 		int freq[]=new int[7];
-
+        
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the number of dice roll-ups:");
+		int numOfRollups=input.nextInt();
 
 		Random rand=new Random();
-		for(int roll=1;roll<=100;roll++){
+		for(int roll=1;roll<=numOfRollups;roll++){
 			freq[1+rand.nextInt(6)]++;
 		}
 
